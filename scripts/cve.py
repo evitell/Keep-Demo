cd52e5a8bfcd387fe334167e074ba6394861b437
evitell <vitelle16@gmail.com>
2025-09-15 13:17:21 +0000

    scripts/cve.py: change to function

--- scripts/cve.py
+++ scripts/cve.py
@@ -3,20 +3,29 @@ import xml.etree.ElementTree as ET
 import io
 
 URL = "https://cve.circl.lu/recent/cvelistv5.rss"
+KEYS = ["guid", "title", "link", "description"]
 
-response = requests.get(URL)
 
-data_s = response.text
-title = None
-description = None
-data = ET.parse(io.StringIO(data_s))
-for x in data.findall("./channel/item"):
-    for y in x:
-        # print(y.tag,y.text)
-        if y.tag == "title":
-            title = y.text
-        elif y.tag == "description":
-            description = y.text
-    break
+def fetch_cves():
+    response = requests.get(URL)
 
-print(f"{title}:\t{description}")
+    data_s = response.text
+    data = ET.parse(io.StringIO(data_s))
+
+    cves = []
+    for x in data.findall("./channel/item"):
+        cve = {}
+
+        for y in x:
+            for key in KEYS:
+                if y.tag == key:
+                    cve[key] = y.text
+
+        cves.append(cve)
+    return cves
+
+
+if __name__ == "__main__":
+    cves = fetch_cves()
+    for cve in cves:
+        print(cve)
