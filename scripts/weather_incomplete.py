b267d7ce0b9c97ffe9b6a15fabf4285edcc9c593
evitell <vitelle16@gmail.com>
2025-09-16 09:29:01 +0000

    inclompete weather send script rev 1

---
+++ scripts/weather_incomplete.py
@@ -0,0 +1,41 @@
+import send
+import weather
+import random
+import json
+import requests
+import time
+
+
+def send_incomplete(loc_name, lat_long):
+    try:
+        w = weather.get_weather(lat_long)
+    except requests.exceptions.ReadTimeout:
+        time.sleep(1)
+        w = weather.get_weather(loc)
+    except:
+        return
+
+    current = w["current"]
+    t = current["time"]
+
+    data = {
+        "time": t,
+        "location": loc_name,
+    }
+    for k in ["temperature_2m", "relative_humidity_2m", "wind_speed_10m"]:
+        if random.choice([True, False]):
+            data[k] = current[k]
+
+    send.send(
+        name=f"Weather in {loc_name}",
+        message=json.dumps(data),
+        description='',
+        source=["incomplete_weather_source"],
+        alert_url=f"https://en.wikipedia.org/{loc_name}",
+        labels={}
+    )
+
+
+if __name__ == "__main__":
+    for loc_name, loc in [("Stockholm", weather.STOCKHOLM), ("Gothenburg", weather.GOTHENBURG), ("Karlsruhe", weather.KARLSRUHE)]:
+        send_incomplete(loc_name, loc)
